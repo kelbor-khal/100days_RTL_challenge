d110d5252d673c452d8f95546b19dfc91b5ae0aa
kelbor-khal <FilinIvan199930@gmail.com>
2024-03-20 19:10:35 +0300

    added johnson counter

--- simulation.sh
+++ simulation.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "Available modules to simulation: "
-echo "0 - clock divider (day 0); "
+echo "0 - clock divider   (day 0); "
+echo "1 - johnson counter (day 1); "
 read -p "Choose module to run simulation: " number_script;
 
 # start modelsim
 case $number_script in
     0) vsim -do 0_day_clock_divider/clock_divider.tcl;;
+    1) vsim -do 1_day_johnson_counter/johnson_counter.tcl;;
     *) echo "This script is not available. Please choose a different one";;
 esac
 
